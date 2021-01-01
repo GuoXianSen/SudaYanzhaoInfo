609e4930a573beb2a53ea9aac64603463e38b1fb
GuoXianSen <775741842@qq.com>
2021-01-02 00:29:22 +0800

    修正原代码文件

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# SudaYanzhaoInfo
-苏州大学研招网信息爬取
+# 苏州大学研招网信息
+
+## 介绍
+
+为了方便大家进行研招网信息查看，写了这个Python脚本，利用`pyinstaller`已经打包完成，生成`.exe`文件在Windows电脑上直接运行。
+
+## 使用
+
+
+
+SudaYanzhaoInfo
+
+## 运行截图
+
+双击运行苏大研招信息小助手V1.0.3即可查看最近五条信息。
+
+![image-20210102002334053](https://gitee.com/clay_guo/pic-bed/raw/master/img/image-20210102002334053.png)
--- src/SudaYanZhao_needtoedit.py
+++ src/SudaYanZhao_needtoedit.py
@@ -2,45 +2,60 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-print("=============================")
-print("Author:B站&YouTube：Clay_Guo")
-print("=============================")
+from src.mail2me import mail
 
 url = "http://yjs.suda.edu.cn/8365/list1.htm"  # 苏大研究生招生网页
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/84.0.4147.105 Safari/537.36 "
+
+}
+
 news = []  # 存储每一条信息以及信息的日期
-# date = []
 urls = []  # 存储信息的网页链接
 
-r = requests.get(url)
+r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.content, 'html.parser')
 
 contents = soup.find_all('ul', attrs={"class": "title-list title-list-light"})
+
+# print(contents)
 
 lianjie = []
 
 for new in contents:
     news.append(new.text)
     lianjie = new.find_all('a')
-    # print(new.get_text())
 
 for url in lianjie:
     urls.append("http://yjs.suda.edu.cn" + url['href'])
     # print("http://yjs.suda.edu.cn/"+ url['href'])
+
+# print(urls[0])
+# print(new.get_text())
+# print("http://yjs.suda.edu.cn/"+new.a['href'])
+
+
+# urls = soup.find_all()
 
 datas = news[0].split('\n\n')[1:-1]
 
 # print(datas)
 
 select = datas[:5]
-a = 0
-for data in select:
-    print(data + urls[a])
-    a = a + 1
+
+ret = mail(select[0] + urls[0] + select[1] + urls[1] + select[2] + urls[2] + select[3] + urls[3] + select[4] + urls[4])
+if ret:
+    print("邮件发送成功")
+else:
+    print("邮件发送失败")
+# for data in select:
+#     print(data)
 
 # print(news)
 
-os.system('pause')
+# os.system('pause')
 
 # for new in news:
 #     print(new)
